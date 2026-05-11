5388c2b23f4e458e172d4cf3f303109342a38102
dwipayogi <dwipayogi50@gmail.com>
2026-05-11 13:41:14 +0700

    chore: update expo dependencies and add expo-network to package.json style: format Colors.ts for consistency and add danger color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,9 +1,10 @@
 export const colors = {
-    primary: '#0060DE',
-    secondary: '#EFF6FF',
-    tertiary: '#F0FAFF',
-    white: '#FFFFFF',
-    black: '#000000',
-    gray: '#666666',
-    lightGray: '#BEC8D2',
-}
+  primary: "#0060DE",
+  secondary: "#EFF6FF",
+  tertiary: "#F0FAFF",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#666666",
+  lightGray: "#BEC8D2",
+  danger: "#D14343",
+};
